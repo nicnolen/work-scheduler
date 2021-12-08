c76fc821e438e988669ef161412790c6e212cf63
Nicholas Nolen <nicnolen@ymail.com>
2021-12-08 13:37:05 -0600

    added in event listener layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
 // GLOBAL VARIABLES
-var today = document.getElementById('currentDay');
+// Reference the paragraph that holds the current date and time
+var todayEl = document.getElementById('currentDay');
+
+// Reference the whole task and buttons
+var timeBlockEl = document.getElementById('time-block');
+var editBtnEl = document.getElementById('edit-button');
+var deleteBtnEl = document.getElementById('delete-button');
+
+// Reference to just the task (not the time)
+var task = document.getElementById('task')
 
 // REFERENCES
 // Access moments.js and format it to say the current day of the week, date, and time
 var rightNow = moment().format('LLLL');
-
 // Display the current date and time on the paragraph with an id of "currentDay"
-today.innerHTML = rightNow;
+todayEl.innerHTML = rightNow;
+
+// FUNCTIONS
+// Edit task function
+var editTask = function() {
+
+}
+
+var deleteTask = function() {
+
+};
+
+// EVENT LISTENERS
+// Capture edit button click
+deleteBtnEl.addEventListener('click', deleteTask);
+
+
