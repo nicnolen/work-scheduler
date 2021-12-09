eb19ac848b7cd2f0e35cdb7124bda072924f0974
Nicholas Nolen <nicnolen@ymail.com>
2021-12-09 12:27:49 -0600

    changed to jQuery and added in an event listener for localStorage, age and an if else statement to color code the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,68 @@
 // GLOBAL VARIABLES
-// Reference the paragraph that holds the current date and time
-var todayEl = document.getElementById('currentDay');
-
-// Array for each hour 
-var hours = [
-    {
-        hour: 8,
-        meridiem: 'AM' // meridiem sets AM and PM
-    },
-    {
-        hour: 9,
-        meridiem: 'AM' // meridiem sets AM and PM
-    },
-    {
-        hour: 10,
-        meridiem: 'AM' // meridiem sets AM and PM
-    },
-    {
-        hour: 11,
-        meridiem: 'AM' // meridiem sets AM and PM
-    },
-    {
-        hour: 12,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-    {
-        hour: 1,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-    {
-        hour: 2,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-    {
-        hour: 3,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-    {
-        hour: 4,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-    {
-        hour: 5,
-        meridiem: 'PM' // meridiem sets AM and PM
-    },
-];
-
-// Array that stores the todo's
-var toDoArr = [];
+// Reference moment.js
+var today = moment();
+// reference the whole task (time and textarea)
+var timeBlockEl = document.querySelector('.container');
 
 // REFERENCES
-// Access moments.js and format it to say the current day of the week, date, and time
-var rightNow = moment().format('LLLL');
 // Display the current date and time on the paragraph with an id of "currentDay"
-todayEl.innerHTML = rightNow;
+$('#currentDay').text(today.format('LLLL')); // LLLL shows day of the week, date and time
 
-// FUNCTIONS
-// Dynamically create time block
-for (var i = 0; i < hours.length; i++) {
-    // reference the whole task (time and textarea)
-    var timeBlockEl = document.querySelector('.container');
-    // create a div for the time
-    var scheduleTimeEl = document.createElement('div');
-    // create a paragraph to store the time
-    var timeEl = document.createElement('p');
-    // create a div to store the tasks
-    var scheduleTaskEl = document.createElement('div');
-    // create a textarea to write the task
-    var taskEl = document.createElement('textarea');
-    // create a div for the save icon
-    var saveTaskEl = document.createElement('div');
-    // create the save task icon
-    var saveTaskIconEl = document.createElement('i');
+// EVENT LISTENER
+// Event listener for saveBtn click
+$('.saveBtn').on('click', function () {
+  // get nearby values of the description in jQuery
+  var textValue = $(this).siblings('.description').val();
+  // get the id attribute of the parent div element
+  var timeKey = $(this).parent().attr('id');
 
-    // style the time div
-    scheduleTimeEl.className = 'd-flex justify-content-center align-items-center col-lg-1 hour';
-    // style the task div
-    scheduleTaskEl.className = 'col-lg-10 past row text';
-    // style the textarea
-    taskEl.className = 'col-lg-12 row text textarea';
-    // style the save icon div
-    saveTaskEl.className = 'd-flex justify-content-center align-items-center col-lg-1 saveBtn';
-    // create the save icon
-    saveTaskIconEl.className = 'fas fa-save';
+  // save in local storage
+  localStorage.setItem(timeKey, textValue);
+});
 
-    // dynamically make the time for the time block referencing the hours array
-    timeEl.textContent = hours[i].hour + hours[i].meridiem;
-    // helper text for the textarea content
-    taskEl.textContent = 'Enter task';
+// Get item from local storage if any
+$('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
 
-    // append the time, task and save divs to the container. NOTE using append allows you to append multiple things at once
-    timeBlockEl.append(scheduleTimeEl, scheduleTaskEl, saveTaskEl);
-    // append the time to the time div
-    scheduleTimeEl.appendChild(timeEl);
-    // append textarea (for the task) to the task div
-    scheduleTaskEl.appendChild(taskEl)
-    // append the save icon to its div
-    saveTaskEl.appendChild(saveTaskIconEl);
-    
+// Function to track tasks and make them change colors if they are in the past, present or future
+function auditTask() {
+  // get current number of hours
+  var currentHour = today.hours();
+
+  // loop over each time block
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour")[1]);
+
+    // if the time Id attribute is before the current hour, add the past class
+    if (timeId < currentHour) {
+      $(this).addClass('past');
+    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+    else if (timeId === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
 }
 
-// Store data in local storage
-var setStorage = function() {
-   toDoArr.push(JSON.parse(localStorage.getItem('toDoArr')));
-   localStorage.setItem('taskEl', JSON.stringify(toDoArr));
-}
-
-// Check due dates
-var auditTasks = function auditTask() {
-
-};
-
-
-// EVENT LISTENERS
-// Capture add button click
-saveTaskIconEl.addEventListener('click', setStorage);
+// Call the audit task function
+auditTask();
 
 // Use setTimeout to update the time every minute (1000ms * 60s)
-setTimeout(function() {
-    // clear the current URL
-    location = ''; // location references the current URL
+setTimeout(function () {
+  // clear the current URL
+  location = ''; // location references the current URL
 }, 1000 * 60);
